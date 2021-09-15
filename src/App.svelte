bc0d25cb50cebda6ccfeced1a628f8c9661db7e3
sammy-code98 <sammysmart98@yahoo.com>
2021-09-15 01:57:32 +0100

    clean up

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,20 @@
 <script>
-	export let name;
+  import { onMount } from "svelte";
+  //   run this function once the lifecyle method happens
+  onMount(() => {
+    grabUsers();
+  });
+  // grab users from github
+  function grabUsers() {
+    fetch("https://api.github.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<main />
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
